3758c99ba1567b31cdeb94ebf6476fde299a3f35
kekoa428 <thomaskekoafarr@gmail.com>
2020-01-21 19:15:27 -0800

    Revert https to http for heroku crash

--- app/models/concerns/api_client.rb
+++ app/models/concerns/api_client.rb
@@ -13,7 +13,7 @@ class ApiClient
   private
 
   def base_route
-    Rails.env.production? ? 'https://population-analyzer.herokuapp.com/' : 'https://population-analyzer-staging.herokuapp.com/'
+    Rails.env.production? ? 'http://population-analyzer.herokuapp.com/' : 'http://population-analyzer-staging.herokuapp.com/'
   end
 
   def build_route
