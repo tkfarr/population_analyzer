ebbd739d94de1c71c6aceb55769971256e5df202
kekoa428 <thomaskekoafarr@gmail.com>
2020-01-21 20:07:14 -0800

    ChartKick.js - install and implement into view

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -10,11 +10,11 @@ class ZipCode < ApplicationRecord
         #{code} AS zip,
         msas.name AS msa,
         cbsas.code AS cbsa,
-        msas.pop_estimate_2015 AS pop2015,
-        msas.pop_estimate_2014 AS pop2014,
-        msas.pop_estimate_2013 AS pop2013,
+        msas.pop_estimate_2011 AS pop2011,
         msas.pop_estimate_2012 AS pop2012,
-        msas.pop_estimate_2011 AS pop2011
+        msas.pop_estimate_2013 AS pop2013,
+        msas.pop_estimate_2014 AS pop2014,
+        msas.pop_estimate_2015 AS pop2015
       FROM msas
       JOIN cbsas ON cbsas.id = msas.cbsa_id
       WHERE msas.lsad = 'Metropolitan Statistical Area'
