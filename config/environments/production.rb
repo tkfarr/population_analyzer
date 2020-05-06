60d74b1e37ab1bdd2e197d6bd23259efe55086ae
kekoa428 <thomaskekoafarr@gmail.com>
2020-05-06 15:02:08 -0700

    Add smtp to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "population_analyzer_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
